bfacce6586e3deb5d9ba0a92f47357200eb58a48
dwipapapa <83564308+dwipapapa@users.noreply.github.com>
2021-08-25 16:17:47 +0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,15 @@
+
+
+namespace Math {
+    //% weight=100 color=#c261ff
+    //% block
+    export function is_even(num:number): boolean {
+        if (num % 2 == 0) {
+        
+        return true
+
+        }
+        
+        return false
+    }
+}
