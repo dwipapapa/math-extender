3e90c6f78932cfa58869305e2b6ff405c5dcf419
dwipapapa <83564308+dwipapapa@users.noreply.github.com>
2021-08-26 09:46:48 +0700

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,15 +1,22 @@
 
-
+//% weight=100 color=#c261ff
 namespace Math {
-    //% weight=100 color=#c261ff
     //% block
     export function is_even(num:number): boolean {
         if (num % 2 == 0) {
         
         return true
-
+        
         }
         
         return false
+    }
+    export function is_odd(num:number): boolean {
+        if (num % 2 == 0) {
+            return false
+        }
+        
+        
+        return true
     }
 }
